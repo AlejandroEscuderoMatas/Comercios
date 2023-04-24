198fa474cdf48fad65c06de43d3d338c6da4dc50
AlejandroEscuderoMatas <alex.escudero1100@gmail.com>
2023-04-24 18:33:55 +0200

    Updated routes and petitions of commerces
    Added validations of token and admin user to commerces routes and tested.
    TO DO bug of commerces post crashing server if unique values are already in database.

--- routes/commerce.js
+++ routes/commerce.js
@@ -1,19 +1,22 @@
 const express = require("express")
 const router = express.Router()
+
 const { getCommerces, getCommerce, createCommerce, updateCommerce, deleteCommerce } = require("../controllers/commerce")
+
 const {validatorCreateCommerce, validatorGetCommerce} = require("../validators/commerce")
 const authMiddleware = require("../middleware/session")
+const checkRol       = require("../middleware/rol")
 
-router.get("/", getCommerces)
+router.get("/", authMiddleware, checkRol(["admin"]), getCommerces)
 
-router.get("/:id", validatorGetCommerce, getCommerce)
+router.get("/:id", authMiddleware, checkRol(["admin"]), validatorGetCommerce, getCommerce)
 
-router.post("/", validatorCreateCommerce, createCommerce)
+router.post("/", authMiddleware, checkRol(["admin"]), validatorCreateCommerce, createCommerce)
 
 //authMiddleware: que tenga el token de sesion
 //checkRol: comprobar que somos rol admin
-router.put("/:id", validatorGetCommerce, validatorCreateCommerce, updateCommerce)
+router.put("/:id", authMiddleware, checkRol(["admin"]), validatorGetCommerce, validatorCreateCommerce, updateCommerce)
 
-router.delete("/:id", validatorGetCommerce, deleteCommerce)
+router.delete("/:id", authMiddleware, checkRol(["admin"]), validatorGetCommerce, deleteCommerce)
 
 module.exports = router
